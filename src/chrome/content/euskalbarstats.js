a18ea86471dd8b51acbd6b8d2962496434391f62
Asier Sarasua Garmendia <asier.sarasua@gmail.com>
2007-07-04 15:42:50 +0000

    Estatistika lokalak ezabatzeko botoi bat sortu da estatistiken leihoan
    svn path=/trunk/; revision=89

--- src/chrome/content/euskalbarstats.js
+++ src/chrome/content/euskalbarstats.js
@@ -2,12 +2,26 @@
 Developers:Asier Sarasua Garmendia 2007This is Free Software (GPL License)asarasua@vitoria-gasteiz.org
 */
 
+//Hurrengo bi lerroek Euskalbarren direktorio nagusiaren URIa eskuratzen dute
+const idStats = "euskalbar@interneteuskadi.org";
+var extNonStats = Components.classes["@mozilla.org/extensions/manager;1"]
+                    .getService(Components.interfaces.nsIExtensionManager)
+                    .getInstallLocation(idStats)
+                    .getItemLocation(idStats);
 
-/*Estatistiken fitxategia idazten du*/function writeStats(dict) {  var URLStats = extNon.clone();  URLStats.append("html");  URLStats.append("stats.txt");	    /*Estatistiken fitxategia ireki eta irakurri*/  var statfis = Components.classes["@mozilla.org/network/file-input-stream;1"]                        .createInstance(Components.interfaces.nsIFileInputStream);  var statsis = Components.classes["@mozilla.org/scriptableinputstream;1"]                        .createInstance(Components.interfaces.nsIScriptableInputStream);  statfis.init(URLStats, -1, 0, 0);  statsis.init(statfis);  var txtStats = statsis.read(statfis.available());  statsis.close();  statfis.close();
-  /*Estatistiken katea arrai batean sartu, aldatu hiztegiari dagokion balioa eta berriro katea bihurtu*/
+/*Estatistiken fitxategia idazten du*/function writeStats(dict) {  var URLStats = extNonStats.clone();  URLStats.append("html");
+  URLStats.append("stats.txt");    /*Estatistiken fitxategia ireki eta irakurri*/  var statfis = Components.classes["@mozilla.org/network/file-input-stream;1"]                        .createInstance(Components.interfaces.nsIFileInputStream);  var statsis = Components.classes["@mozilla.org/scriptableinputstream;1"]                        .createInstance(Components.interfaces.nsIScriptableInputStream);  statfis.init(URLStats, -1, 0, 0);  statsis.init(statfis);  var txtStats = statsis.read(statfis.available());  statsis.close();  statfis.close();
   statsArray = txtStats.split(",");
-  var zkia = statsArray[dict]*1;
-  statsArray[dict] = zkia+1;
+  if(dict == -1){
+    /*Estatistikak garbitu*/
+    for (n in statsArray){
+      statsArray[n] = 0;
+    }
+  }else{
+    /*Estatistiken katea arrai batean sartu, aldatu hiztegiari dagokion balioa eta berriro katea bihurtu*/
+    var zkia = statsArray[dict]*1;
+    statsArray[dict] = zkia+1;
+  }
   txtStats = statsArray.join(",");
 
   /*Fitxategia idatzi*/
@@ -16,4 +30,24 @@ Developers:Asier Sarasua Garmendia 2007This is Free Software (GPL License)asa
   statout.init(URLStats,0x02 | 0x08 | 0x20, 0664, 0);
   statout.write(txtStats, txtStats.length);
   statout.flush();
-  statout.close();}	
+  statout.close();
+
+  if(dict == -1){
+    //Leihoko lerro guztietan zero zenbakia idatzi (estatistikak garbitu)
+    var tree = document.getElementById("zuhaitza");
+    var colstats = tree.columns.getNamedColumn("stats-count");
+    for (f=0;f<=17;f++){
+      tree.view.setCellText(f,colstats,0);
+    }  }
+}
+
+/*Estatistikak leihoan erakusten ditu*/
+function setBalioak() {
+  var statsText = window.arguments[0];
+  statsArray = statsText.split(",");
+  var tree = document.getElementById("zuhaitza");
+  var colstats = tree.columns.getNamedColumn("stats-count");
+  for (f in statsArray){
+    tree.view.setCellText(f,colstats,statsArray[f]);
+  }
+}
