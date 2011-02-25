e65f7052147930d6a8f3709296025b768cd99803
Julen Ruiz Aizpuru <julenx@gmail.com>
2011-02-25 19:40:12 +0000

    New i18n/l10n function: _().
    This function first tries to get a translation for the passed string in
    the current bundle. If no string is found, then it tries to fallback to
    the en-US string. Finally, if neither of the methods succeeded, it
    returns the given string but only starting since the last dot.
    
    So for instance if there are no translations for
    _('euskalbar.menu.General'), the function will return 'General' as a
    very last fallback string.
    
    svn path=/trunk/; revision=286

--- src/chrome/content/lib.js
+++ src/chrome/content/lib.js
@@ -3,6 +3,14 @@ var euskalbarLib = {};
 
 (function() {
 
+  /* Constants */
+
+  const chromeregCID = '@mozilla.org/chrome/chrome-registry;1';
+  const chromeregIID = Ci.nsIChromeRegistry;
+
+  const resphIID = Ci.nsIResProtocolHandler;
+
+
   /*
    * DOM manipulation
    */
@@ -29,5 +37,100 @@ var euskalbarLib = {};
 
     return text;
   };
+
+
+  /*
+   * i18n / l10n
+   */
+
+  this._ = function (text) {
+    var strKey = text.replace(' ', '_', "g").toLowerCase();
+
+    // First try getting the current locale's translation
+    try {
+      var bundle = this.$("leuskal");
+      return bundle.getString(strKey);
+    } catch(e) {
+      console.log("Failed to get translation for key: " + strKey);
+    }
+
+    // If it fails, fall back to en-US
+    try {
+      var bundle = this.getDefaultStrBundle();
+      return bundle.GetStringFromName(strKey);
+    } catch (err) {
+      console.log("Failed to get translation for key: " + strKey);
+    }
+
+    // As a last resource, we use the given text after the last dot
+    var index = text.lastIndexOf(".");
+    if (index > 0 && text.charAt(index - 1) != "\\") {
+      text = text.substr(index + 1);
+    }
+
+    text = text.replace("_", " ", "g");
+
+    return text;
+  };
+
+  this.defaultStrBundle = null;
+
+  this.getDefaultStrBundle = function () {
+    if (!this.defaultStringBundle) {
+      var bundleURL = "chrome://euskalbar/locale/euskalbar.properties";
+      var fileURI = this.FileIO.getLocalOrSystemPath(bundleURL);
+
+      var parts = fileURI.split("/");
+      parts[parts.length - 2] = "en-US";
+      console.log(parts.join('/'));
+      this.defaultStrBundle = Services.strings.createBundle(parts.join("/"));
+    }
+
+    console.log(this.defaultStrBundle);
+    return this.defaultStrBundle;
+  };
+
+
+  /*
+   * File IO operations
+   */
+
+  this.FileIO = {
+
+      getLocalSystemURI: function (url) {
+        try {
+          var uri = Services.io.newURI(url, null, null);
+
+          if (uri.schemeIs("resource")) {
+            var ph = Services.io.getProtocolHandler("resource").QueryInterface(resphIID);
+            var abspath = ph.getSubstitution(uri.host);
+            uri = Services.io.newURI(uri.path.substr(1), null, abspath);
+          }
+
+          if (uri.schemeIs("chrome")) {
+            var chromeRegistry = Cc[chromeregCID].getService(chromeregIID);
+            uri = chromeRegistry.convertChromeURL(uri);
+          }
+
+          return uri;
+        } catch(exc) {
+        }
+      },
+
+      /* Gets path starting from a URL */
+      getLocalOrSystemPath: function (url, allowDirectories) {
+        var uri = this.getLocalSystemURI(url);
+
+        if (uri instanceof Ci.nsIFileURL) {
+          var file = uri.file;
+          if (allowDirectories) {
+            return file && file.path;
+          } else {
+            return file && !file.isDirectory() && file.path;
+          }
+        }
+      },
+
+    };
 
 }).apply(euskalbarLib);
