15713f332544f2713c2e44c16b5979a4ff41f2b0
Julen Ruiz Aizpuru <julenx@gmail.com>
2011-03-01 17:44:29 +0000

    Add proper licensing information (GPLv3) + AUTHORS file.
    svn path=/trunk/; revision=326

--- src/chrome/content/dicts.js
+++ src/chrome/content/dicts.js
@@ -1,13 +1,22 @@
-﻿// Developers: Juanan Pereira, Asier Sarasua Garmendia 2006
-//             Julen Ruiz Aizpuru, Asier Sarasua Garmendia 2007
-//             Igor Leturia Azkarate 2008
-//             Chetan Thapliyal 2009
-// This is Free Software (GPL License)
-// juanan@diariolinux.com
-// asarasua@vitoria-gasteiz.org
-// julenx@gmail.com
-// ileturia@gmail.com
-// chetan.thapliyal@discreteguidepost.in
+﻿/*
+ * Euskalbar - A Firefox extension for helping in Basque translations.
+ * Copyright (C) 2006-2011 Euskalbar Taldea (see AUTHORS file)
+ *
+ * This file is part of Euskalbar.
+ *
+ * Euskalbar is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 with (euskalbarLib) {
 
