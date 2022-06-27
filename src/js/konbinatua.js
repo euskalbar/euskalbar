9b4ddff36b83d1dddf11bcfaeff67ee9e58addd3
Thadah D. Denyse <thadahdenyse@protonmail.com>
2022-06-27 23:01:11 +0200

    Fix #269 Shift+Enter portaera aldatzen da

--- src/js/konbinatua.js
+++ src/js/konbinatua.js
@@ -2,154 +2,95 @@
 
 function serialize(obj,encoding)
 {
-    var serializatua;
-    // Latin-1, ASCII eta UTF-8 kodeketentzako serializazioa
-    if (encoding=='latin-1')
+    var serializatua = '';
+    if (obj.length > 0)
     {
-        serializatua=encodeURIComponent(obj).replace(/%([0-9A-F]{2})/g,function(_match,p1)
+        if (encoding=='latin-1')
         {
-            return String.fromCharCode(parseInt(p1,16));
-        });
-    }
-    else if (encoding=='ascii' || encoding=='utf-8')
-    {
-        serializatua=encodeURIComponent(obj).replace(/%([0-9A-F]{2})/g,function(_match,p1)
-        {
-            return String.fromCharCode(parseInt(p1,16));
+            serializatua=''+Object.keys(obj).reduce(function(a,k){a.push(k+'='+escape(obj[k]));return a},[]).join('&')
         }
-        ).replace(/[^\x20-\x7E]/g,function(match)
+        else if (encoding=='ascii')
         {
-            return '%'+match.charCodeAt(0).toString(16).toUpperCase();
+            serializatua=''+Object.keys(obj).reduce(function(a,k){a.push(k+'='+obj[k].normalize('NFD').replace(/[\u0300-\u036f]/g,""));return a},[]).join('&')
         }
-        );
-    }
-    else
-    {
-        serializatua=encodeURIComponent(obj);
+        else
+        {
+            serializatua=''+Object.keys(obj).reduce(function(a,k){a.push(k+'='+encodeURIComponent(obj[k]));return a},[]).join('&')
+        }
     }
     return serializatua;
 }
 
 /* Baliabide baten XHR eskaera egiten du */
 
-function makexhr(baliabidea,urlosoa,params,opts,baliabideak)
+function makexhr(baliabidea,urlosoa,params,opts)
 {
 
     // XHR eskaera sortu
-
-    var xhr=new XMLHttpRequest();
-
-    // Erantzuna datorrenean
-
-    xhr.onreadystatechange=function()
-    {
-        if (xhr.readyState==XMLHttpRequest.DONE)
-        {
-            if (xhr.status==200)
-            {
-
-                // Ikusi zein baliabidetakoa zen, URLa begiratuta
-
-                for (const baliabide of baliabideak)
-                {   
-                    baliabidea=baliabideendatuak[baliabide];
-                    var url=baliabidea.getUrl(opts);
-                    params=baliabidea.getParams(opts);
-                    urlosoa='';
-                    if (baliabidea.method=='GET')
-                    {
-                        if (serialize(params,baliabidea.encoding)!=='')
-                        {
-                            urlosoa=url+'?'+serialize(params,baliabidea.encoding);
-                        }
-                        else
-                        {
-                            urlosoa=url;
-                        }
-                    }
-                    else
-                    {
-                        urlosoa=url;
-                    }
-                    if (encodeURI(urlosoa).replace(/%25/g,'%').replace('https','http')==encodeURI(xhr.responseURL).replace(/%25/g,'%').replace('https','http'))
-                    {
-                        // Dagokion zutabean idatzi emaitza
-
-                        document.getElementById('Gorputza'+baliabidea.name).innerHTML=baliabidea.scrap(xhr.responseText,opts);
-                    }
-                }
-            }
-        }
-    };
+    var xhr;
+    if (window.XMLHttpRequest){
+        xhr = new XMLHttpRequest();  
+    }
 
     // XHR eskaera ireki
 
-    xhr.open(baliabidea.method,urlosoa,true);
-
-    // GET bada
+    xhr.open(baliabidea.method, urlosoa, true);
 
     if (baliabidea.method=='GET')
     {
-
-        // XHR eskaera bidali
-
         xhr.send();
     }
-
-    // POST bada
-
     else
     {
-
-        // Parametroak bidali
-
         xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-
-        // XHR eskaera bidali
-
-        xhr.send(serialize(params,baliabidea.encoding));
+        xhr.send(params);
     }
-    
+
+    xhr.addEventListener('load', function() {
+        if (xhr.status == 200) {
+            document.getElementById('Gorputza'+baliabidea.name).innerHTML=baliabidea.scrap(xhr.responseText,opts);
+        }
+    });    
 }
 
 /* Baliabideen zutabeak sortzen ditu */
 
 function KargatuBaliabideak(request,_sender,_sendResponse)
-{
-
+{    
+ 
     // Orriari izenburua jarri
 
     document.title=request['izenburua'];
 
-    // Baliabide bakoitzeko
+    // Baliabide bakoitzeko zutabeak sortu
 
     var baliabideak=request['baliabideak'];
     for (const baliabide of baliabideak)
     {
         var baliabidea=baliabideendatuak[baliabide];
-
-        // Zutabea sortu
-
-        var tha=document.createElement('th');
-        tha.setAttribute('id','Name'+baliabidea.name);
-        document.getElementById('buruak').appendChild(tha);
-        var texta=document.createTextNode(baliabidea.description);
-        tha.appendChild(texta);
-        var tdo=document.createElement('td');
-        tdo.setAttribute('id','Oina'+baliabidea.name);
-        tdo.setAttribute('class','gorputza oharra');
-        document.getElementById('oinak').appendChild(tdo);
-        var aa=document.createElement('a');
-        aa.setAttribute('href',baliabidea.homePage);
-        aa.setAttribute('target','_blank');
-        tdo.appendChild(aa);
-        var atexta=document.createTextNode(baliabidea.description);
-        aa.appendChild(atexta);
-        var tdg=document.createElement('td');
-        tdg.setAttribute('id','Gorputza'+baliabidea.name);
-        tdg.setAttribute('class','gorputza');
-        document.getElementById('gorputzak').appendChild(tdg);
-
+        
+        if (!document.getElementById('Name'+baliabidea.name)){
+            var tha=document.createElement('th');
+            tha.setAttribute('id','Name'+baliabidea.name);
+            document.getElementById('buruak').appendChild(tha);
+            var texta=document.createTextNode(baliabidea.description);
+            tha.appendChild(texta);
+            var tdo=document.createElement('td');
+            tdo.setAttribute('id','Oina'+baliabidea.name);
+            tdo.setAttribute('class','gorputza oharra');
+            document.getElementById('oinak').appendChild(tdo);
+            var aa=document.createElement('a');
+            aa.setAttribute('href',baliabidea.homePage);
+            aa.setAttribute('target','_blank');
+            tdo.appendChild(aa);
+            var atexta=document.createTextNode(baliabidea.description);
+            aa.appendChild(atexta);
+            var tdg=document.createElement('td');
+            tdg.setAttribute('id','Gorputza'+baliabidea.name);
+            tdg.setAttribute('class','gorputza');
+            document.getElementById('gorputzak').appendChild(tdg);
+        }
+        
         // Baliabidearen parametroak irakurri
 
         var opts={
@@ -178,11 +119,9 @@ function KargatuBaliabideak(request,_sender,_sendResponse)
         }
 
         // XHR eskaera egin
-
-        makexhr(baliabidea,urlosoa,params,opts,baliabideak);
+        makexhr(baliabidea,urlosoa,params,opts);
     }
 }
 
 // Baliabideak kargatzeko mezua jasotzen denean, egin
-
 browser.runtime.onMessage.addListener(KargatuBaliabideak);
